6fd77369d7bf1d3bce7b1041822c2e5dc06380a8
Tudor <112909151+tudor-seserman@users.noreply.github.com>
2023-05-14 19:00:37 -0500

    Passes tests and works better

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -47,22 +47,17 @@ public class HomeController {
 
     @PostMapping("add")
     public String processAddJobForm(@ModelAttribute @Valid Job newJob,
-                                       Errors errors, Model model, @RequestParam(required = false) int employerId, @RequestParam(required = false) List<Integer> skills) {
+                                       Errors errors, Model model, @RequestParam int employerId, @RequestParam List<Integer> skills) {
 
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Job");
             return "add";
         }
-/*
-        THIS MAKES THE TEST FAIL BUT SITE TO WORK BETTER!
- */
-//        Optional<Employer> optEmployer = employerRepository.findById(employerId);
-//        if (optEmployer.isEmpty()) {
-//            model.addAttribute("title", "Add Job");
-//            return "add";
-//        }
-//        Employer employer = optEmployer.get();
-//        newJob.setEmployer(employer);
+
+        Optional<Employer> optEmployer = employerRepository.findById(employerId);
+        if (optEmployer.isPresent()) {
+        Employer employer = optEmployer.get();
+        newJob.setEmployer(employer);}
 
         List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
         newJob.setSkills(skillObjs);
